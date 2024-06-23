5bb7fb267ae1a4d0ce06f91b47812934b41c4886
Lyumih <m.ivanov1225@gmail.com>
2024-06-23 20:56:58 +0300

    i

---
+++ app/app.view.ts
@@ -0,0 +1,13 @@
+namespace $.$$ {
+	/** 0 - белые, 1 - чёрные */
+	export class $tale_app extends $.$tale_app {
+	// 	@$mol_mem
+	// 	user() {
+	// 		return this.realm().home().hall_by( $go_user_dao, $hyoo_crus_rank_public )
+	// 	}
+
+	// 	user_id() {
+	// 		return this.user()?.ref().description ?? ''
+	// 	}
+	}
+}
---
+++ ui/card/card.view.css.ts
@@ -0,0 +1,13 @@
+namespace $ {
+
+	const { px } = $mol_style_unit
+
+	$mol_style_define( $tale_ui_card, {
+		border: {
+			width: px( 1 ),
+			style: 'solid',
+			color: $mol_theme.hover
+		}
+	} )
+
+}
