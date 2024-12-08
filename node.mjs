801c8b625e4757cf44f45acbb56d881024caac72
Lyumih <Lyumih@users.noreply.github.com>
2024-12-08 07:23:14 +0000

    Deploying to gh-pages from @ Lyumih/tale@37623ccc793c543a305bfb30d06bdb685ea8d33d 🚀

--- node.mjs
+++ node.mjs
@@ -7558,7 +7558,7 @@ var $;
             display: 'block',
             position: 'relative',
             font: {
-                family: 'monospace',
+                family: 'inherit',
             },
             Numb: {
                 textAlign: 'right',
